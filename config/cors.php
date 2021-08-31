b40a5371aec2f6cbe5f900df717c7829c6b76beb
Cedric <l3c43v1039p@gmail.com>
2021-08-31 21:51:39 +0800

    cors update

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
+    'paths' => [],
+    'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
     'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+    'allowed_origins_patterns' => ['http://localhost:8080', 'https://simopolicy.vercel.app'],
+    'allowed_headers' => ['X-Custom-Header', 'Upgrade-Insecure-Requests', '*'],
+    'exposed_headers' => false,
+    'max_age' => false,
+    'supports_credentials' => false,
 
 ];
