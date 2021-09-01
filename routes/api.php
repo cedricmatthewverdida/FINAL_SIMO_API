55cb399576c095a16f96aa69eb72c1d75c7450ed
Cedric <l3c43v1039p@gmail.com>
2021-09-01 16:11:14 +0800

    Updates

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,10 +21,10 @@ Route::post('register',[
     'register'
 ]);
 
-Route::post('login',[
-    \App\Http\Controllers\AuthController::class,
-    'login'
-]);
+// Route::post('login',[
+//     \App\Http\Controllers\AuthController::class,
+//     'login'
+// ]);
 
 Route::get('alluser',[
     \App\Http\Controllers\AuthController::class,
@@ -32,15 +32,25 @@ Route::get('alluser',[
 ]);
 
 
-Route::middleware('auth:sanctum')->group(function (){
-    Route::get('user',[
-        \App\Http\Controllers\AuthController::class,
-        'user'
-    ]);
-    Route::post('logout',[
-        \App\Http\Controllers\AuthController::class,
-        'logout'
-    ]);
+// Route::middleware('auth:sanctum')->group(function (){
+//     Route::get('user',[
+//         \App\Http\Controllers\AuthController::class,
+//         'user'
+//     ]);
+//     Route::post('logout',[
+//         \App\Http\Controllers\AuthController::class,
+//         'logout'
+//     ]);
+// });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('admin')->group(function(){
+    Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
+    Route::post('/logout', [LoginController::class, 'logout']);
+
 });
 
 
